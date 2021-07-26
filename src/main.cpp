9c708d6f5224f229e33f6b00a0143705b32d9c2d
yuwei Xu <850565178@qq.com>
2021-07-26 22:32:16 +0800

    add readme.md && dynamic_reconfiguration && nodehandle

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,13 @@
 #include <ros/ros.h>
 #include "lidar_test/lidarParse.hpp"
 int main(int argc,char** argv){
-    ros::init(argc,argv,"xyw_test");
-    ros::NodeHandle nh;
-    lidarParse lp(nh,argc,argv);
+    ros::init(argc,argv,"xyw_lidar_test_node");
+    ros::NodeHandle nh("~");
+    xyw_lidar_test::lidarParse lp(nh,argc,argv);
     ros::Rate f(10);
-    while (ros::ok())
-    {
         // lp.toSeeGaussDistributionShape();
-        ros::spinOnce();
-        f.sleep();
-    }
+    ros::spin();
+    
 
     
 }
