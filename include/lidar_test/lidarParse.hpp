65f6268ae8f87f1d6449be92e404241b6977a035
FishInWave <850565178@qq.com>
2022-01-05 22:49:04 +0800

    test cov rotation and eigen noalias

--- include/lidar_test/lidarParse.hpp
+++ include/lidar_test/lidarParse.hpp
@@ -46,6 +46,7 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include "KITTI_helper.hpp"
 #include "so3/so3.hpp"
+#include "pcl_helper.hpp"
 using namespace std;
 
 using PointT = pcl::PointXYZI;
@@ -237,12 +238,87 @@ namespace xyw_lidar_test
             // testQuateRotate();
             // testApprox();
             // cout << testEigenTopoint<pcl::PointXYZ>() << endl;
-            testMap();
+            // testMap();
+            // testCovRotate();
+            testEigenNoalias();
+        }
+        // http://eigen.tuxfamily.org/dox/classEigen_1_1MatrixBase.html#a2c1085de7645f23f240876388457da0b
+        // Eigen乘法默认是启用eval的，即默认存在混淆。实际上，只要一个变量同时在两侧出现，那么就必须解决混淆。
+        // 即仅当两侧没有相同数据存在时，才可以用noalias来提速,也应当提速。
+        void testEigenNoalias(){
+            Eigen::Matrix4f m;
+            m << 0.998989, -0.0393154 ,-0.0217818 ,17.2709 ,0.0396042, 0.999131 ,0.0129876 ,0.953767, 0.0212523,-0.0138372 ,0.999678, 0.45526,0,0,0,1;
+            Eigen::Matrix4f n(m);
+            Eigen::Matrix4f o(m);
+            tic::TicTocPart t;
+            m.noalias() = m*m;
+            cout << t.toc() <<endl;
+            cout << "m*m.noalias: \n" << m << endl;
+            t.toc();
+            n = n*n;
+            cout << t.toc() <<endl;
+            cout << "n*n:\n" << n << endl;
+            t.toc();
+            o = (o * o).eval();
+            cout << t.toc() <<endl;
+            cout << "eval*eval():\n" << o << endl;
+        }
+        void testCovRotate(){
+            Eigen::Matrix4f m;
+            m << 0.998989, -0.0393154 ,-0.0217818 ,17.2709 ,0.0396042, 0.999131 ,0.0129876 ,0.953767, 0.0212523,-0.0138372 ,0.999678, 0.45526,0,0,0,1;
+            PointCloud::ConstPtr cloud_cptr = cloud_in;
+
+            PointCloud after_rotate;
+            std::vector<Eigen::Matrix4d, Eigen::aligned_allocator<Eigen::Matrix4d>> source_covs;
+            std::vector<Eigen::Matrix4d, Eigen::aligned_allocator<Eigen::Matrix4d>> after_covs;
+            calculate_covariances(cloud_cptr,source_covs,20,16);
+            
+            pcl::transformPointCloud(*cloud_in,after_rotate,m);
+            PointCloud::ConstPtr rotate_cptr(new PointCloud(after_rotate));
+            calculate_covariances(rotate_cptr,after_covs,20,16);
+
+            for(int i = 0; i < 10 ; i++){
+                cout << "source_covs: \n" << source_covs[i] << endl;
+                cout << "rotate_covs: \n" << after_covs[i] << endl;
+                cout << "R*cov*R^T: \n" << m*source_covs[i].cast<float>()*m.transpose() << endl;
+            }
+
         }
         // map删除需要避免自增，且若要遍历删除必须使用for而不是range for
         // https://blog.csdn.net/weixin_34258078/article/details/92290970
+        // swap是有效的
         void testMap()
         {
+            unordered_map<string, int> um;
+            unordered_map<string, int> um_temp;
+            um.emplace(pair<string, int>("a", 1));
+            um.emplace(pair<string, int>("b", 1));
+            um.emplace(pair<string, int>("c", 1));
+            um_temp.emplace(pair<string, int>("d", 1));
+            um_temp.emplace(pair<string, int>("e", 1));
+            swap(um, um_temp);
+            cout << "um: " << endl;
+            for (auto i : um)
+            {
+                cout << i.first << " " << i.second << endl;
+            }
+            cout << "um_temp: " << endl;
+            for (auto i : um_temp)
+            {
+                cout << i.first << " " << i.second << endl;
+            }
+            um.swap(um_temp);
+            cout << "um: " << endl;
+            for (auto i : um)
+            {
+                cout << i.first << " " << i.second << endl;
+            }
+            cout << "um_temp: " << endl;
+            for (auto i : um_temp)
+            {
+                cout << i.first << " " << i.second << endl;
+            }
+
             std::map<string, int> map;
             map["a"] = 1;
             map["b"] = 2;
@@ -306,11 +382,14 @@ namespace xyw_lidar_test
         // Eigen::Matrix可以用C类型的指针或者数组初始化
         void testEigenForceTrans()
         {
-            double a[6] = {1, 2, 3, 4, 5, 6};
-            Eigen::Matrix<double, 3, 2> m(a);
-            cout << m << endl;
+            double a[6] = {1.1, 2.1, 3.4, 4.5, 5.6, 6.4};
+            Eigen::Matrix<double, 2, 3> m(a);
+            Eigen::Vector4d v(a);
+            cout << m.cast<int>() << endl;
             Eigen::Map<Eigen::Quaterniond> q(a);
             cout << q.w() << endl;
+            Eigen::Vector3i s_a = v.head<3>().cast<int>();
+            cout << s_a << endl;
         }
         void testSO3()
         {
---
+++ include/pcl_helper.hpp
@@ -0,0 +1,34 @@
+#ifndef PCL_HELPER_HPP
+#define PCL_HELPER_HPP
+#include "omp.h"
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/search/kdtree.h>
+template <typename PointT>
+void calculate_covariances(typename boost::shared_ptr<const pcl::PointCloud<PointT>> &cloud,
+                           std::vector<Eigen::Matrix4d, Eigen::aligned_allocator<Eigen::Matrix4d>> &covariances,int k_correspondences_, int num_threads_)
+{
+    pcl::search::KdTree<PointT> kdtree;
+    kdtree.setInputCloud(cloud);
+    covariances.resize(cloud->size());
+// TODO 计算协方差时，没有进行距离判断，而是直接取了最近的10个点，这会导致scan-to-scan时劣化。
+#pragma omp parallel for num_threads(num_threads_) schedule(guided, 8)
+    for (int i = 0; i < cloud->size(); i++)
+    {
+        std::vector<int> k_indices;
+        std::vector<float> k_sq_distances;
+        kdtree.nearestKSearch(cloud->at(i), k_correspondences_, k_indices, k_sq_distances);
+
+        Eigen::Matrix<double, 4, -1> neighbors(4, k_correspondences_);
+        for (int j = 0; j < k_indices.size(); j++)
+        {
+            neighbors.col(j) = cloud->at(k_indices[j]).getVector4fMap().template cast<double>();
+        }
+
+        neighbors.colwise() -= neighbors.rowwise().mean().eval();
+        Eigen::Matrix4d cov = neighbors * neighbors.transpose() / k_correspondences_;
+
+        covariances[i] = cov;
+    }
+}
+#endif
